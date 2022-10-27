d2de1a15c3e2dfa806c79c3d5a8c4f65de875266
赵润晓 <zhaorunxiao@sics.ac.cn>
2022-10-27 11:24:27 +0800

    Optimize the class FColumnName using char array instead of String.

--- src/main/java/com/sics/rock/tableinsight4/table/column/FColumnName.java
+++ src/main/java/com/sics/rock/tableinsight4/table/column/FColumnName.java
@@ -3,6 +3,7 @@ package com.sics.rock.tableinsight4.table.column;
 import com.sics.rock.tableinsight4.utils.FAssertUtils;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * typedef String ColumnName
@@ -12,11 +13,14 @@ import java.io.Serializable;
  */
 public class FColumnName implements Serializable {
 
-    public final String columnName;
+    public final char[] columnName;
+
+    private final int hashCode;
 
     public FColumnName(String columnName) {
         FAssertUtils.require(columnName != null, "ColumnName cannot be null");
-        this.columnName = columnName;
+        this.columnName = columnName.toCharArray();
+        this.hashCode = columnName.hashCode();
     }
 
     @Override
@@ -24,16 +28,16 @@ public class FColumnName implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FColumnName that = (FColumnName) o;
-        return columnName.equals(that.columnName);
+        return hashCode == that.hashCode && Arrays.equals(columnName, that.columnName);
     }
 
     @Override
     public int hashCode() {
-        return columnName.hashCode();
+        return hashCode;
     }
 
     @Override
     public String toString() {
-        return columnName;
+        return String.valueOf(columnName);
     }
 }
